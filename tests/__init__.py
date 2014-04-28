d447615c3630bca986cbc8da5061bef5d1544dcb
wcdolphin <wcdolphin@gmail.com>
2014-04-28 00:09:35 -0400

    Removes unnecessary imports in __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,10 +8,3 @@
     :copyright: (c) 2014 by Cory Dolphin.
     :license: MIT, see LICENSE for more details.
 """
-
-from base_test import *
-from test_credentials import *
-from test_headers import *
-from test_options import *
-from test_origins import *
-from test_w3 import *
