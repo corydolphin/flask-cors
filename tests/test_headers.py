187046cc05676c30029a2827917ce02c4a95f33e
wcdolphin <wcdolphin@gmail.com>
2014-07-22 16:04:02 -0700

    Refactors testing for app-wide configuration of headers and origins

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -16,9 +16,15 @@ except:
     # support local usage without installed package
     from flask_cors import *
 
+
 class HeadersTestCase(FlaskCorsTestCase):
     def setUp(self):
         self.app = Flask(__name__)
+
+        @self.app.route('/test_default')
+        @cross_origin()
+        def test_default():
+            return 'Welcome!'
 
         @self.app.route('/test_list')
         @cross_origin(headers=["Foo", "Bar"])
@@ -35,22 +41,11 @@ class HeadersTestCase(FlaskCorsTestCase):
         def test_set():
             return 'Welcome!'
 
-    def test_app_configured_headers(self):
-        ''' If the application contains a list of headers in the
-            `CORS_HEADERS` config value, then headers should default to them.
-        '''
-        app = Flask(__name__)
-        app.config['CORS_HEADERS'] = ['Foo', 'Bar']
-
-        @app.route('/')
-        @cross_origin(methods=['GET', 'OPTIONS', 'HEAD', 'PUT', 'POST'])
-        def wildcard():
-            return 'Welcome!'
-
-        with app.test_client() as c:
-            for verb in self.iter_verbs(c):
-                result = verb('/')
-                self.assertEqual(result.headers.get(ACL_HEADERS), 'Foo, Bar')
+    def test_default(self):
+        with self.app.test_client() as c:
+            result = c.get('/test_default')
+            self.assertTrue(result.headers.get(ACL_HEADERS) is None,
+                            "Default should have no allowed headers")
 
     def test_list_serialized(self):
         ''' If there is an Origin header in the request,
@@ -78,6 +73,23 @@ class HeadersTestCase(FlaskCorsTestCase):
             allowed = result.headers.get(ACL_HEADERS)
             # Order is not garaunteed for sets
             self.assertTrue(allowed in ['Foo, Bar', 'Bar, Foo'])
+
+
+class AppConfigHeadersTestCase(FlaskCorsTestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['CORS_HEADERS'] = ['Foo', 'Bar']
+
+        @self.app.route('/')
+        @cross_origin(methods=['GET', 'OPTIONS', 'HEAD', 'PUT', 'POST'])
+        def wildcard():
+            return 'Welcome!'
+
+    def test_list_serialized(self):
+        with self.app.test_client() as c:
+            for verb in self.iter_verbs(c):
+                result = verb('/')
+                self.assertEqual(result.headers.get(ACL_HEADERS), 'Foo, Bar')
 
 
 if __name__ == "__main__":
