06c61dfa328054658eef07f6d2c6c06bf06de2f2
CoryDolphin <CoryDolphin@gmail.com>
2015-05-07 23:15:21 -0400

    Bump version to 2.0.1

--- tests/decorator/test_duplicate_headers.py
+++ tests/decorator/test_duplicate_headers.py
@@ -28,62 +28,6 @@ class AllowsMultipleHeaderEntries(FlaskCorsTestCase):
     def test_multiple_set_cookie_headers(self):
         resp = self.get('/test_multiple_set_cookie_headers')
         self.assertEqual(len(resp.headers.getlist('set-cookie')), 2)
-            # self.assertTrue(resp.headers.get(ACL_ALLOW_HEADERS) is None,
-            #                 "Default should have no allowed headers")
-
-    # def test_allow_headers_no_request_headers(self):
-    #     '''
-    #     No ACL_REQUEST_HEADERS sent, ACL_ALLOW_HEADERS should be empty
-    #     '''
-    #     resp = self.preflight('/test_allow_headers', origin='www.example.com')
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS), None)
-
-    # def test_allow_headers_with_request_headers(self):
-    #     '''
-    #         If there is an Access-Control-Request-Method header in the request
-    #         and Access-Control-Request-Method is allowed for cross origin
-    #         requests and request method is OPTIONS, and every element in the
-    #         Access-Control-Request-Headers is an allowed header, the
-    #         Access-Control-Allow-Headers header should be echoed back.
-    #     '''
-    #     resp = self.preflight('/test_allow_headers',
-    #                           origin='www.example.com',
-    #                           cors_request_headers=['X-Example-Header-A'])
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS),
-    #                      'X-Example-Header-A')
-
-    # def test_allow_headers_with_unmatched_request_headers(self):
-    #     '''
-    #         If every element in the Access-Control-Request-Headers is not an
-    #         allowed header, then the matching headers should be returned.
-    #     '''
-    #     resp = self.preflight('/test_allow_headers',
-    #                           origin='www.example.com',
-    #                           cors_request_headers=['X-Not-Found-Header'])
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS), None)
-
-    #     resp = self.preflight('/test_allow_headers',
-    #                           origin='www.example.com',
-    #                           cors_request_headers=['X-Example-Header-A',
-    #                                                 'X-Not-Found-Header'])
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS),
-    #                      'X-Example-Header-A')
-
-    # def test_allow_headers_regex(self):
-    #     '''
-    #         If every element in the Access-Control-Request-Headers is not an
-    #         allowed header, then the matching headers should be returned.
-    #     '''
-    #     resp = self.preflight('/test_allow_headers_regex',
-    #                           origin='www.example.com',
-    #                           cors_request_headers=['X-COMPANY-FOO'])
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS), 'X-COMPANY-FOO')
-
-    #     resp = self.preflight('/test_allow_headers_regex',
-    #                           origin='www.example.com',
-    #                           cors_request_headers=['X-Not-Found-Header'])
-    #     self.assertEqual(resp.headers.get(ACL_ALLOW_HEADERS), None)
-
 
 if __name__ == "__main__":
     unittest.main()
