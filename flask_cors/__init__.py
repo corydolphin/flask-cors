db744fd0d70ade529b1c77f75640a2f75aca75b2
Cory Dolphin <corydolphin@gmail.com>
2015-11-07 23:12:57 -0500

    Reduces logging verbosity and makes it simpler to disable logging

--- flask_cors/__init__.py
+++ flask_cors/__init__.py
@@ -13,3 +13,14 @@ from .extension import CORS
 from .version import __version__
 
 __all__ = ['CORS', 'cross_origin']
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
