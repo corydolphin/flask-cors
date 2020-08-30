53e284dc873d1260f5de1262a6e24f9d0783aa86
Cory Dolphin <cory@nextdoor.com>
2020-08-30 13:59:37 -0600

    Fix import

--- flask_cors/extension.py
+++ flask_cors/extension.py
@@ -10,7 +10,11 @@
 """
 from flask import request
 from .core import *
-from urllib.parse import unquote_plus
+try:
+    from urllib.parse import unquote_plus
+except ImportError:
+    from urllib import unquote_plus
+
 LOG = logging.getLogger(__name__)
 
 class CORS(object):
