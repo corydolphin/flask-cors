0ab2beabbe5686b3fea389e6c6d2e11733c1008c
nakanoi <nakanoinryota@gmail.com>
2023-04-17 03:40:12 +0900

    Add invalid CORS response code test for CORS, decorator
    Add invalid CORS reseponse code tests for both CORS and
    decorator configurations.
    Each tests are for 'without options', 'valid options'(which
    is the case that invalid_cors_response_code is set to
    Client Error Code), and 'invalid options'.
    
    Signed-off-by: nakanoi <nakanoinryota@gmail.com>

--- tests/extension/test_app_extension.py
+++ tests/extension/test_app_extension.py
@@ -378,5 +378,102 @@ class AppExtensionBadRegexp(FlaskCorsTestCase):
             self.assertEqual(resp.status_code, 200)
 
 
+class AppExtensionInvalidCorsResponseStatusCode(FlaskCorsTestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        CORS(self.app, resources={
+            r'/with_no_options': {
+                'origins': {'http://valid.com'}
+            },
+            r'/with_client_error_status': {
+                'origins': {'http://valid.com'},
+                'invalid_cors_status_code': 403
+            },
+            r'/with_non_client_error_response': {
+                'origins': {'http://valid.com'},
+                'invalid_cors_status_code': 304
+            },
+        })
+
+        @self.app.route('/with_no_options')
+        def with_no_options():
+            return 'NO OPTIONS!'
+
+        @self.app.route('/with_client_error_status')
+        def with_client_error_status():
+            return 'WITH 403!'
+
+        @self.app.route('/with_non_client_error_response')
+        def with_non_client_error_response():
+            return 'WITH 304!'
+
+    def test_with_no_options(self):
+        ''' If no `invalid_cors_status_code` options set,
+            response code OK(200) will be returned
+            when invalid CORS request.
+        '''
+        for index, resp in enumerate(self.iter_responses(
+                '/with_no_options',
+                verbs=['get', 'head', 'options'],
+                origin='http://valid.com'
+            )):
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), 'http://valid.com')
+            expected = u'NO OPTIONS!' if index == 0 else u''
+            self.assertEqual(resp.data.decode('utf-8'), expected)
+
+        for index, resp in enumerate(self.iter_responses(
+                '/with_no_options',
+                verbs=['get', 'head', 'options'],
+                origin='http://invalid.com'
+            )):
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), None)
+            expected = u'NO OPTIONS!' if index == 0 else u''
+            self.assertEqual(resp.data.decode('utf-8'), expected)
+
+    def test_with_client_error_status(self):
+        ''' If `invalid_cors_status_code` options set as 403,
+            which is Clinet Error Status Code, response code
+            FORBIDDEN(403) will be returned when invalid CORS
+            request.
+        '''
+        for index, resp in enumerate(self.iter_responses(
+                '/with_client_error_status',
+                verbs=['get', 'head', 'options'],
+                origin='http://valid.com'
+            )):
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), 'http://valid.com')
+            expected = u'WITH 403!' if index == 0 else u''
+            self.assertEqual(resp.data.decode('utf-8'), expected)
+
+        for resp in self.iter_responses('/with_client_error_status', origin='http://invalid.com'):
+            self.assertEqual(resp.status_code, 403)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), None)
+            self.assertEqual(resp.data.decode('utf-8'), u'')
+
+    def test_with_non_client_error_status(self):
+        ''' If `invalid_cors_status_code` options set as 200,
+            which is not Clinet Error Status Code, response
+            code OK(200) will be returned when invalid CORS
+            request.
+        '''
+        for index, resp in enumerate(self.iter_responses(
+                '/with_non_client_error_response',
+                verbs=['get', 'head', 'options'],
+                origin='http://valid.com'
+            )):
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), 'http://valid.com')
+            expected = u'WITH 304!' if index == 0 else u''
+            self.assertEqual(resp.data.decode('utf-8'), expected)
+
+        for resp in self.iter_responses('/with_non_client_error_response', origin='http://invalid.com'):
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.headers.get(ACL_ORIGIN), None)
+            self.assertEqual(resp.data.decode('utf-8'), u'')
+
+
 if __name__ == "__main__":
     unittest.main()
